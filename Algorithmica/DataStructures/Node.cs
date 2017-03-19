b0e991bdc751cdd7d1c952b636134d466b3589bb
Rafal Czabaj <czabajrafal@gmail.com>
2017-03-19 13:37:09 +0100

    Converting project to VS 2017 version

---
+++ Algorithmica/DataStructures/Node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algorithmica.DataStructures
+{
+	public abstract class Node <T>
+	{
+		public T Data { get; private set; }
+
+		public Node(T data)
+		{
+			Data = data;
+		} 
+	}
+}
--- Algorithmica/Sorting/BubbleSort.cs
+++ Algorithmica/Sorting/BubbleSort.cs
@@ -12,17 +12,22 @@ namespace Algorithmica.Sorting
 			if (input == null) throw new ArgumentNullException();
 			if (input.Length == 1) return input;
 
-			for (int outer_it = 0; outer_it < input.Length; ++outer_it)
+			bool changed;
+
+			for (int outer_it = input.Length - 1; outer_it >= 0; --outer_it)
 			{
-				for (int nested_it = 0; nested_it < input.Length - 1; ++nested_it)
+				changed = false;
+				for (int nested_it = 0; nested_it < outer_it; ++nested_it)
 				{
 					if (input[nested_it] > input[nested_it + 1])
 					{ 
 						int temp = input[nested_it];
 						input[nested_it] = input[nested_it + 1];
 						input[nested_it + 1] = temp;
+						changed = true;
 					}
 				}
+				if (!changed) break;
 			}
 
 			return input;
