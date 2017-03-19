6e00c9f7fb4ad7eb87c6a65b6c8075886578a73c
Rafal Czabaj <czabajrafal@gmail.com>
2017-03-19 14:26:14 +0100

    Adding Basic Stack operations

---
+++ Algorithmica/DataStructures/Stack.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithmica.DataStructures
+{
+	public class Stack<TItem>
+	{
+		protected List<TItem> dataStorage;
+		private int currentIndex = 0;
+
+		public Stack()
+		{
+			dataStorage = new List<TItem>();
+		}
+
+		public Stack(IEnumerable<TItem> initialData)
+		{
+			dataStorage = new List<TItem>(initialData);
+			currentIndex = dataStorage.Count - 1;
+		}
+
+		public TItem Peek()
+		{
+			throw new NotImplementedException();
+		}
+		public TItem Pop()
+		{
+			if (IsEmpty()) throw new InvalidOperationException();
+
+			TItem item = dataStorage[currentIndex];
+			dataStorage.RemoveAt(currentIndex);
+			currentIndex--;
+			return item;
+		}
+
+		public bool TryPop(out TItem item)
+		{
+			if (IsEmpty())
+			{
+				item = default(TItem);
+				return false;
+			}
+			
+			item = dataStorage[currentIndex];
+			dataStorage.RemoveAt(currentIndex);
+			currentIndex--;
+			return true;
+		}
+
+		public void Push()
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool IsEmpty()
+		{
+			return dataStorage.Count == 0;
+		}
+	}
+}
